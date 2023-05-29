3d9b6448edcfee310623c120152e6dbf36d74145
evan <you@example.com>
2023-05-29 18:22:28 +0200

    feat: ajoutcreation de l'entity Question, ajout d'une question dans la bdd

--- src/Form/QuestionType.php
+++ src/Form/QuestionType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Question;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -21,6 +22,7 @@ class QuestionType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            'data_class' => Question::class,
             // Configure your form options here
         ]);
     }
